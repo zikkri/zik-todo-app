10dc35bdf007d8a4eb8af98dd5b64d6f6140e409
zack <zbowker66@gmail.com>
2023-05-08 14:13:16 -0400

    made some progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zik-todo-app';
+  loggedIn: boolean = false;
+  public static userLoggedIn: any;
+  constructor(private auth: AngularFireAuth) {}
+  ngOnInit(): void {
+    const loggedInStatus = localStorage.getItem('loggedIn');
+    if (loggedInStatus && loggedInStatus === 'true') {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.loggedIn = true;
+        const uid = user.uid;
+        AppComponent.userLoggedIn = user;
+        const username = user?.displayName;
+        // console.log(AppComponent.userLoggedIn);
+        localStorage.setItem('userID', uid);
+        if (username != null) {
+          localStorage.setItem('username', username);
+        }
+        // console.log(uid);
+      } else {
+        this.loggedIn = false;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,19 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ListComponent } from './pages/list/list.component';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from './environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [AppComponent, SigninComponent, SignupComponent, ListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
